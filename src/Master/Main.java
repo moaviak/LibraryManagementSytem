f42e1a65c494e4f033cbac8f107911d9faf72702
moavia <mmoavia2002@gmail.com>
2022-12-03 18:28:32 +0500

    Admin Account Initialization

--- src/Master/Main.java
+++ src/Master/Main.java
@@ -2,8 +2,26 @@ package Master;
 import java.util.*;
 
 public class Main {
+    static Scanner sc = new Scanner(System.in);
 
     public static void main(String[] args) {
-	// write your code here
+	    ArrayList<Admin> admins = new ArrayList<>();
+        System.out.println("-------------------------------------");
+        System.out.println("Welcome To Library Management System");
+        System.out.println("-------------------------------------");
+
+        System.out.println("\nInitialize Admin Account");
+
+        System.out.print("Enter Name: ");
+        String adminName = sc.nextLine();
+        System.out.print("Enter Age: ");
+        int adminAge = Integer.parseInt(sc.nextLine());
+        System.out.print("Enter Email: ");
+        String adminEmail = sc.nextLine();
+        System.out.print("Enter Password: ");
+        String adminPassword = sc.nextLine();
+        admins.add(new Admin(adminName, adminAge, adminEmail, adminPassword));
+
+        System.out.println("\nAdmin Account Successfully Created");
     }
 }
